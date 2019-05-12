9bed023fd5b5e89396a0a6270d51f7f32779215b
AhmedHIbrahim <ah1.6.1998@gmail.com>
2019-05-13 00:07:37 +0300

    HW3

---
+++ assignments/2_MBR_GRUPconf/grubConf.py
@@ -0,0 +1,20 @@
+import os 
+#Reading and writing MBR to myMBR
+os.system("dd if=/dev/sda of=./myMBR bs=512 count=1 | hexdump -C")  
+
+
+#Show content of myMBR
+#-> os.system("file ./myMBR")
+
+
+#Entering a new entery in GRUB with an iso file under my home directory
+with open("/etc/grub.d/40_custom", "a") as editedGrubFile:
+    editedGrubFile.write("menuentry \"Ubuntu Live ISO\" { \n")
+    editedGrubFile.write("set UbuntuISOFile=\"/home/ahmed/ubuntu-18.04.2-desktop-amd64.iso\" \n")
+    editedGrubFile.write("loopback loop (hd7,1)$UbuntuISOFile}")
+
+
+#Runing the new Operating system on GRUB entirely on the ram
+# https://wiki.ubuntu.com/BootToRAM
+
+
