61eca889c3b2bd53cef4b18a7c38537d099160ea
AhmedHIbrahim <ah1.6.1998@gmail.com>
2019-05-13 10:39:17 +0300

    mbr_grub_conf_v2

--- assignments/2_MBR_GRUPconf/grubConf.py
+++ assignments/2_MBR_GRUPconf/grubConf.py
@@ -1,3 +1,4 @@
+#This file must be called by sudo privilege to function
 import os 
 #Reading and writing MBR to myMBR
 os.system("dd if=/dev/sda of=./myMBR bs=512 count=1 | hexdump -C")  
@@ -12,6 +13,9 @@ with open("/etc/grub.d/40_custom", "a") as editedGrubFile:
     editedGrubFile.write("menuentry \"Ubuntu Live ISO\" { \n")
     editedGrubFile.write("set UbuntuISOFile=\"/home/ahmed/ubuntu-18.04.2-desktop-amd64.iso\" \n")
     editedGrubFile.write("loopback loop (hd7,1)$UbuntuISOFile}")
+
+os.system("update-grub")
+
 
 
 #Runing the new Operating system on GRUB entirely on the ram
